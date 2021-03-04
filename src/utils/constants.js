09afbec185c8b971bc236a43d1b5e2888cf6f21c
Helder Vieira <helder.zv@gmail.com>
2021-03-04 18:02:52 +0100

    React files

---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+const CTFULL_DATA_URL = "https://graphql.contentful.com/content/v1/spaces/kp6wmoapi8iy/explore?access_token=rj5s9hjur3wHY27I5p7NROBQbZp4GcxQcEMQ1iThM44";
+export default CTFULL_DATA_URL;
